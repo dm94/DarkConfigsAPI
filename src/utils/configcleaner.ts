17ab4f1dbac5cd3e576235a4aef7eca23a0e74b5
dm94 <dm94@hotmail.es>
2023-09-20 21:21:10 +0200

    1.0.2: Added more modules for clean

--- src/utils/configcleaner.ts
+++ src/utils/configcleaner.ts
@@ -1,15 +1,30 @@
 import { ConfigFile, PluginInfo } from "@/types/configfile";
 
-export const unnecesaryFeatureInfo = ["eu.darkbot.popcorn.def.UserAgentUpdater"];
-export const oldFeatures = ["com.deeme.modules.PallladiumHangar", "eu.darkbot.drmalito.minicore.MiniTrain", "com.deeme.modules.PaladiumModule", "eu.darkbot.leanon00.Main.Features.GGSpinner", "com.deeme.tasks.Discord"];
+export const unnecesaryFeatureInfo = [
+    "eu.darkbot.popcorn.def.UserAgentUpdater",
+];
+
+export const oldFeatures = [
+    "com.deeme.modules.PallladiumHangar",
+    "eu.darkbot.drmalito.minicore.MiniTrain",
+    "com.deeme.modules.PaladiumModule",
+    "eu.darkbot.leanon00.Main.Features.GGSpinner",
+    "com.deeme.tasks.Discord",
+    "com.botorbit.darkplane.features.chaosalienmodule.ChaosAlienModule",
+    "com.botorbit.darkplane.extrasplus.ExtrasPlus",
+    "com.deeme.modules.AstralGate",
+];
+
 export const oldModules = ["com.deemetool"];
 export const blackListModules = ["fernando.qmieasputodemrd"];
 
+export const oldPluginsByName = ["Dmplugin_by_Dm94Dani", "Schifo_by_Fabio"];
+
 export const leakInfoFromFeatues = [
-    "com.deeme.tasks.discord.Discord.discordWebHook", 
-    "com.deeme.tasks.WeeklySchedule.profile1.hangarId", 
-    "com.deeme.tasks.WeeklySchedule.profile2.hangarId", 
-    "com.deeme.tasks.WeeklySchedule.profile3.hangarId", 
+    "com.deeme.tasks.discord.Discord.discordWebHook",
+    "com.deeme.tasks.WeeklySchedule.profile1.hangarId",
+    "com.deeme.tasks.WeeklySchedule.profile2.hangarId",
+    "com.deeme.tasks.WeeklySchedule.profile3.hangarId",
     "com.deeme.tasks.WeeklySchedule.profile4.hangarId",
     "com.deeme.modules.PalladiumHangar.collectHangar",
     "com.deeme.modules.PalladiumHangar.sellHangar",
@@ -23,7 +38,7 @@ export const leakInfoFromFeatues = [
     "eu.darkbot.leanon00.Main.Features.DiscordTask.discordWebHook",
     "com.pikapika.tasks.nova.Nova.NOVA_INFO",
     "com.pikapika.tasks.nova.Nova.roster",
-    "com.pikapika.tasks.nova.Nova.resource"
+    "com.pikapika.tasks.nova.Nova.resource",
 ];
 
 export const unnecesaryInfoFromFeatures = [
@@ -38,7 +53,7 @@ export const unnecesaryInfoFromFeatures = [
     "com.pikapika.behaviour.eventConfigChanger.EventConfigChanger.labyrinth.lastCheck",
     "com.pikapika.behaviour.eventConfigChanger.EventConfigChanger.plutus.lastCheck",
     "com.pikapika.behaviour.eventConfigChanger.EventConfigChanger.npc_event.lastCheck",
-    "com.pikapika.behaviour.eventConfigChanger.EventConfigChanger.world_boss_event.lastCheck"
+    "com.pikapika.behaviour.eventConfigChanger.EventConfigChanger.world_boss_event.lastCheck",
 ];
 
 
@@ -93,6 +108,10 @@ export const getEnabledFeatures = (config: ConfigFile): string[] => {
     let enableFeatures: string[] = [];
 
     keys.forEach((key) => {
+        if (oldPluginsByName.includes(key)) {
+            return;
+        }
+
         const pluginInfoValue: PluginInfo = pluginInfo[key];
         if (pluginInfoValue.ENABLED_FEATURES && pluginInfoValue.ENABLED_FEATURES.length > 0) {
             const features = pluginInfoValue.ENABLED_FEATURES.filter((f) => permitFeature(f));
